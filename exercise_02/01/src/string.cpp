ea5212075c28262cacafe357a5a3e3516ca643ae
Sven Osterwalder <sven.osterwalder@gmail.com>
2014-05-02 18:46:56 +0200

    Implementation of exercise 02-01

--- exercise_02/01/src/string.cpp
+++ exercise_02/01/src/string.cpp
@@ -1,14 +1,34 @@
 #include "string.h"
 
+String::String()
+{
+    std::cout << __FILE__ << " at line " << __LINE__ << ": ";
+    std::cout << "Calling default constructor";
+    std::cout << std::endl << std::endl;
+
+}
+
+String::String(const String& source)
+{
+    std::cout << __FILE__ << " at line " << __LINE__ << ": ";
+    std::cout << " Calling copy-constructor String(String&)";
+    std::cout << std::endl << std::endl;
+
+    this->m_strString = source.m_strString;
+}
+
 String::String(string strInput) {
-    std::cout << "Calling overladed String(str)" << std::endl;
-    std::cout << "m_strString is now: " << strInput << std::endl;
+    std::cout << __FILE__ << " at line " << __LINE__ << ": ";
+    std::cout << "Calling overladed String(string)";
+    std::cout << std::endl << std::endl;
 
     this->m_strString = strInput;
 }
 
 String& String::operator=(const string& strInput) {
-    std::cout << "Calling operator=(string) " << std::endl;
+    std::cout << __FILE__ << " at line " << __LINE__ << ": ";
+    std::cout << "Calling operator=(string&) ";
+    std::cout << std::endl << std::endl;
 
     m_strString = strInput;
 
@@ -17,7 +37,43 @@ String& String::operator=(const string& strInput) {
 
 String& String::operator=(const String& strInput)
 {
-    std::cout << "Calling operator=(String) " << std::endl;
+    std::cout << __FILE__ << " at line " << __LINE__ << ": ";
+    std::cout << "Calling operator=(String&) ";
+    std::cout << std::endl << std::endl;
+
+    m_strString = strInput.m_strString;
+
+    return *this;
+}
+
+void* String::operator new(size_t size)
+{
+    std::cout << __FILE__ << " at line " << __LINE__ << ": ";
+    std::cout << "calling operator new(size_t) ";
+    std::cout << std::endl << std::endl;
+
+    return malloc(size);
+}
+
+void String::operator delete (void* ptr)
+{
+    std::cout << __FILE__ << " at line " << __LINE__ << ": ";
+    std::cout << "calling operator delete(size_t) ";
+    std::cout << std::endl << std::endl;
+
+    free(ptr);
+}
+
+char& String::operator[] (const int nIndex)
+{
+    //TODO: Check index!
+
+    std::cout << __FILE__ << " at line " << __LINE__ << ": ";
+    std::cout << "calling operator [](int): ";
+    std::cout << this->m_strString.at(nIndex);
+    std::cout << std::endl << std::endl;
+
+    return this->m_strString.at(nIndex);
 }
 
 std::ostream& operator<<(std::ostream& s, const String strString) {
--- exercise_02/01/src/string.h
+++ exercise_02/01/src/string.h
@@ -12,12 +12,18 @@ class String
     friend ostream& operator<<(ostream& s, const String* strString);
 
     public:
-        String() { cout << "Default constructor" << endl; }
+        String();
+        String(const String&);
         String(string strInput);
         ~String() {}
 
         String& operator = (const string& strInput);
         String& operator = (const String& strInput);
+
+        void* operator new (size_t size);
+        void operator delete (void* ptr);
+
+        char& operator[] (const int nIndex);
 
     private:
         string m_strString;
